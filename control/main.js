357891d2dea9a218a501ba8ce7056f48570da5f0
Mukhammad Vicky <123183707+Viserys89@users.noreply.github.com>
2026-04-05 01:45:03 +0700

    base untuk routing

--- control/main.js
+++ control/main.js
@@ -1,3 +1,4 @@
+const base = window.location.origin + "/Cairan-Adiktif/";
 const loginForm = document.getElementById("loginForm");
 loginForm.addEventListener("submit", function (event) {
   event.preventDefault();
@@ -5,13 +6,13 @@ loginForm.addEventListener("submit", function (event) {
   // Logika pengecekan email dan arah redirect
   if (emailInput === "host") {
     // Ganti URL ini sesuai dengan path/lokasi file host kamu
-    window.location.href = "..Cairan-Adiktif/host/index.html";
+    window.location.href = base + "host/index.html";
   } else if (emailInput === "admin") {
     // Ganti URL ini sesuai dengan path/lokasi file admin kamu
-    window.location.href = "../admin/index.html";
+    window.location.href = base + "admin/index.html";
   } else if (emailInput === "finance") {
     // Ganti URL ini sesuai dengan path/lokasi file finance kamu
-    window.location.href = "../finance/index.html";
+    window.location.href = base + "finance/index.html";
   } else {
     // Jika input selain host/admin/finance
     alert("Email tidak dikenali! Silakan masukkan: host, admin, atau finance.");
